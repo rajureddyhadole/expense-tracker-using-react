37136161637abf41330b1c00340b7cc93000c071
Hadole Raju <67407713+rajureddyhadole@users.noreply.github.com>
2024-10-19 18:44:54 +0530

    small update

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,17 +1,17 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import { GlobalContext } from './GlobalState';
 
 const Transaction = ({ transaction }) => {
 
-  const {deleteTransaction} = useContext(GlobalContext);
+  const { deleteTransaction } = useContext(GlobalContext);
 
   return (
-    <li className='border-[1px] bg-white shadow-md h-12 mt-2 flex justify-between px-4 items-center font-medium' 
-    id={transaction.amount < 0 ? 'minus' : 'plus'}>
+    <li className='border-[1px] bg-white rounded-md shadow-md h-12 mt-2 flex justify-between px-4 items-center font-medium'
+      id={transaction.amount < 0 ? 'minus' : 'plus'}>
       <span>{transaction.text}</span>
       <span>{transaction.amount}</span>
       <button className='text-xl font-semibold' onClick={() => deleteTransaction(transaction.id)}>x</button>
-      </li>
+    </li>
   )
 }
 
